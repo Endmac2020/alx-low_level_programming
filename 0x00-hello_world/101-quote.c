5461d7d528202ab1c7e50c6bbb103330c076630d
Endmac2020 <nmacendurance@gmail.com>
2023-03-17 15:04:28 -0700

    unix operating system

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main-A c program that prints the piece of art is useful
+ * Return: 1 (success)
+ */
+int main(void)
+{
+	  fput("And that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	  return (1);
+}
