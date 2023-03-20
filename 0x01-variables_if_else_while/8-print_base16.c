49bcace28517737f68b6459ffdc58fda9f0d85db
Endmac2020 <nmacendurance@gmail.com>
2023-03-20 14:17:03 -0700

    hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 
-/*
+/**
  * main - main function
  *
- * Return: always 0
+ * Return: always 0 (correct)
  */
 
-int (main);
-
+int main(void)
 {
 	int i;
 	char j;
-	for (i = 0 ; i < 10 ; i++)
-	      putchar(i + '0');
-	for (j = 'a' ; j <= 'f' ; j++)
-		putchar(j);
-	putchar("\n");
-	return (0);
+
+	for (i = 0; i < 10; i++)
+		putchar(i + '0');
+	for (j = 'a'; j <= 'f'; i++)
+	putchar(j);
+	putchar('\n');
+		return (0);
 }
+
