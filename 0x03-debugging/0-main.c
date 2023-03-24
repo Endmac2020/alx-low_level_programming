3bf9388726144be4b9d55e26a30dc7fab88c64a8
Endmac2020 <nmacendurance@gmail.com>
2023-03-24 16:28:07 -0700

    check4

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - test function that prints if the interger is positive or negative
+ * main - test function that prints if the integer is positive or negative
  * Return: 0
  */
 
@@ -9,7 +9,7 @@ int main(void)
 {
  	int i;
 
- 	i = 0;
+ 	i = 98;
  	positive_or_negative(i);
 
 	return (0);
