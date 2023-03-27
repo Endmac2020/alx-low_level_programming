fb1fcbe1a03918c1a18f8cffc635174ea4240bee
Endmac2020 <nmacendurance@gmail.com>
2023-03-27 15:36:40 -0700

    resetvalue

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/* main - update the value of n
+ *
+ * Return: always 0 correct
+ */
+
+int main(void)
+{
+	int n;
+
+	        n = 110;
+		printf("n=%d\n", n);
+		reset to 98(&n);
+		printf("n=%d\n", n);
+		return (0);
+
+}
