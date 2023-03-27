f6e43a685ea7c3664fb9ca0ea0be33611bcaba18
Endmac2020 <nmacendurance@gmail.com>
2023-03-27 16:36:12 -0700

    swap int

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 
-/* main - update the value of n
+/* reset_to_98 - takes a pointer to a parameter and updates the value it points to to 98
  *
  * @n: value to be updated is 98
  *
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/* swap int: swaps the values of two integers
+ * @a: int *a
+ * @b: int *b
+ *
+ * Return: always 0
+ */
+
+void swap_int(int *a, int *b)
+{
+	swap int
+
+		change = *b;
+	*b = *a;
+	*a = change;
+}	
