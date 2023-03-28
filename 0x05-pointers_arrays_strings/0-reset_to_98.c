3c527297a0bf605fec1f562b1628142f24dc3643
Endmac2020 <nmacendurance@gmail.com>
2023-03-28 03:59:29 -0700

    checks

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
-/* reset_to_98 - updates the value it points to 98
+/** reset to 98 - update the value it points to 98
  *
  * @n: value to be updated is 98
  *
  * Return: always 0 correct
- */
+ */ 
 
 void reset_to_98(int *n)
 
