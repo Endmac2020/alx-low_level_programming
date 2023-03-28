58643ae61f13e76698e80b3b39f3cc5ddd4865d2
Endmac2020 <nmacendurance@gmail.com>
2023-03-28 12:45:04 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+
+/**
+ * _puts - function that prints a string
+ * @str: pointer of the string
+ *
+ * Return - string and a new line
+ */
+
+void _puts(char *str)
+
+{
+	int i = 0;
+
+		while
+			(str[i]);
+	{
+		_putchar(str[i]);
+		i++;
+	}
+		_putchar("\n");
+
+}
