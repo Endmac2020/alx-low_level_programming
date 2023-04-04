1fe20a711c59241f9b9b7e4db6ab389a44850506
Endmac2020 <nmacendurance@gmail.com>
2023-04-03 18:05:23 -0700

    set string

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * print_diagsums - prints the sum of the two diagonals
+ * @a: input
+ * @size: input
+ * Return: always 0 correct
+ */
+
+void print_diagsums(int *a, int size)
+{
+	
+}	
