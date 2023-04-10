cf25198f19e500854badd0bd2cbfb461a6a2229a
Endmac2020 <nmacendurance@gmail.com>
2023-04-10 15:28:38 -0700

    minimum number

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - function that multiplies numbers
+ * @argc: number of argument
+ * @argv: argument vector
+ *
+ * Return: always correct
+ */
+int main(int argc, char *argv[])
+{
+
+}
+
